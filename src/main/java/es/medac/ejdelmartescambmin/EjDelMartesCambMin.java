bc0c5ab9a4ca0a03102a0f52e4ee6e4dee68aeeb
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-03-13 12:44:09 +0100

    Commit inicial del Ejercio del Martes Cambio Mínimo

---
+++ src/main/java/es/medac/ejdelmartescambmin/EjDelMartesCambMin.java
@@ -0,0 +1,13 @@
+
+package es.medac.ejdelmartescambmin;
+
+/**
+ *
+ * @author asier.ruiz
+ */
+public class EjDelMartesCambMin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
