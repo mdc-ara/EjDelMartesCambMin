b0696d07ca6c59184440c8b51d8702b5d4b59861
mdc-ara <149468225+mdc-ara@users.noreply.github.com>
2024-03-13 12:54:18 +0100

    Un único bucle para contar monedas e imprimir

--- src/main/java/es/medac/ejdelmartescambmin/EjDelMartesCambMin.java
+++ src/main/java/es/medac/ejdelmartescambmin/EjDelMartesCambMin.java
@@ -8,6 +8,21 @@ package es.medac.ejdelmartescambmin;
 public class EjDelMartesCambMin {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int precio = 65; // Precio del producto en céntimos
+        int pagado = 100; // Cantidad pagada por el usuario en céntimos
+        calcularCambio(precio, pagado);
+    }
+
+    private static void calcularCambio(int precio, int pagado) {
+        int cambio = pagado - precio;
+        int[] monedas = {25,10,5,1}; // denominaciones de monedas en céntimos
+        int[] cantidad = new int[monedas.length]; // almacenará cuántas monedas de cada tipo
+        
+        System.out.println("Cambio total a devolver " + cambio + " céntimos");
+        for(int i =0; i<monedas.length; i++) {
+            cantidad[i]=cambio/monedas[i]; // división entera
+            cambio %= monedas[i];
+            System.out.println(cantidad[i]+ " monedas de " + monedas [i] + " céntimos");
+        }
     }
 }
